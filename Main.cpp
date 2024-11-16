ab0f38310d2443531bcac61305b3862776771019
Your Name <your-email@example.com>
2024-11-16 07:00:54 -0800

    starting with my lab 32 as base code. currently modifying to implement new functions for lane switching and selecting a new lane.

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,109 @@
-#include <iostream>
+//Anthony Sarabia - Lab 33
 
+//NEW REQUIREMENTS
+//1. Add More Lanes : There are now 4 toll booth lanes at play. Use an array to hold the lanes. The array will be of type deque<Car>. 
+// When the simulation starts, pre-populate the lanes with 1-3 cars like before.Each time period will now have operations happening on all lines.
+//2. Add Switching Lanes : Any car at the end of any queue can switch lanes to a random lane that's not their original lane.
+//3. Probabilities : 46% probability that the car at the head of the queue pays its toll and leaves the queue
+//39 % probability that another car joins the queue
+//15 % probability that the rear car will shift lanes
+//4. Termination Criteria: Run the simulation for 20 time periods.
+
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h"
 using namespace std;
 
+//UPDATED CONSTANTS
+const int QUEUE_SIZE = 2;
+const int PAY_PROB = 55;
+const int JOIN_PROB = 45;
+const int NUMBER_LANES = 4;
+const int SIMULATION_PERIOD = 20;
+
+//FUNCTION PROTOTYPES
+int randomNumber();
+void seedRandom();
+void intializeLanes(deque<Car> lanes[], int initalSize); //initalizes all lanes w specifced # of randomly generated cars
+void simulateCycle(deque<Car>& tollQueue, int cycle);
+void runSimulation();
+void display(deque<Car> lanes[]);
+//need a function for both lane switching and selecting a random lane
+
 int main() {
+	//Seed
+	seedRandom();
+
+	//& run simulation (all i need for main)
+	runSimulation();
 
 	return 0;
+}
+
+int randomNumber() {
+	return rand() % 100 + 1; // random num between 1-100
+}
+void seedRandom() {
+	srand(static_cast<unsigned int>(time(0)));
+}
+void intializeLanes(deque<Car> lanes[], int initalSize) {
+	//Simple for loop to intialize the deque with a specificed number of randomly generated cars/
+	for (int i = 0; i < initalSize; i++) {
+		Car car;
+		tollQueue.push_back(car);
+	}
+}
+void simulateCycle(deque<Car>& tollQueue, int cycle) {
+	cout << endl;
+	cout << "Time: " << cycle << " Operation: " << endl;
+
+	//generate random number here 
+	int randNum = randomNumber();
+
+	// 55% probability: Car pays and leaves
+	if (randNum <= PAY_PROB && !tollQueue.empty()) {
+		Car car = tollQueue.front();
+		tollQueue.pop_front();
+		cout << "Car Paid: ";
+		car.print();
+	}
+	else if (randNum > PAY_PROB && randNum <= PAY_PROB + JOIN_PROB) {
+		Car newCar;
+		tollQueue.push_back(newCar);
+		cout << "Joined Lane:";
+		newCar.print();
+	}
+
+	// 45% probability: New car joins the queue
+
+	//display here 
+	cout << "Queue:" << endl;
+	display(tollQueue);
+}
+void runSimulation() {
+	deque<Car> toll_queue;
+	int cycle = 0;
+
+	// Initialize the queue with the initial number of cars
+	cout << "Initial Queue:" << endl;
+	intializeQueue(toll_queue, QUEUE_SIZE);
+
+	//then display
+	display(toll_queue);
+
+	//and finally, run simulation cycles until the queue is empty
+	while (!toll_queue.empty()) {
+		cycle++;
+		simulateCycle(toll_queue, cycle);
+	}
+
+	cout << "This simulation ran " << cycle << " cycles until the queue was empty; results will vary." << endl;
+}
+void display(deque<Car>lanes[]) {
+	//use auto to display toll_queue
+	for (auto& car : toll_queue) {
+		car.print();
+	}
 }
